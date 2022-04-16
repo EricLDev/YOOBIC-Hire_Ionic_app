dd5bc0bb3d59b6fa5a0c68f5c6440a17775bc907
Eric Lehmann <lehm_eric@yahoo.fr>
2022-04-16 15:57:16 +0200

    init project + Router Nav menu

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Dashboard', url: '/folder/Dashboard', icon: 'apps' },
+    { title: 'Photos', url: '/folder/Photos', icon: 'image' },
+    { title: 'Actors', url: '/folder/Actors', icon: 'people' },
+    { title: 'Films', url: '/folder/Film', icon: 'film' },
+    { title: 'Starships', url: '/folder/Starships', icon: 'rocket' },
+    { title: 'Chat', url: '/folder/Chat', icon: 'chatbox' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel'];
+  constructor() {}
+}
